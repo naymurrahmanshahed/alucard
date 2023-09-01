60d0d650646e90705ccf997e1139662668b26729
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-09-01 21:31:28 +0600

    community section added

--- components/home/Community.tsx
+++ components/home/Community.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Community = () => {
   return (
     <section>
-      <div className=' wrapper section-padding flex h-full w-full items-center justify-center '>
+      <div className=' wrapper section-padding relative flex h-full w-full items-center justify-center '>
         {/* main box */}
         <div className='relative h-[350px]  w-[350px] bg-gray'>
           {/* left box */}
@@ -39,7 +40,36 @@ const Community = () => {
               className='h-full w-full object-cover'
             />
           </div>
+
+          {/* upper right text */}
+          <h2 className='headline-1 absolute -top-[18%] left-[36%] z-[2] whitespace-nowrap font-semibold uppercase '>
+            Join Alucard
+          </h2>
+
+          {/* bottom left text */}
+          <h2 className='headline-1 absolute -bottom-[18%] right-[40%] z-[2] font-semibold uppercase '>
+            Community
+          </h2>
         </div>
+
+        {/* join button */}
+        {/* <Link
+          href={'/'}
+          className='absolute bottom-10 right-10 z-[1] flex h-32 w-32 items-center justify-center rounded-full bg-red text-lg uppercase text-light '
+        >
+          Join Now
+        </Link> */}
+        <Link
+          href={'/#packages'}
+          className='group absolute bottom-10 right-10 z-[1] flex h-32 w-32 -translate-x-1/2 items-center justify-center overflow-hidden rounded-full bg-dark/80 text-lg  uppercase text-light shadow-2xl shadow-dark/50 backdrop-blur-lg hover:shadow-red/50'
+        >
+          <p className='z-[3]'>Join Now</p>
+
+          {/* Filler 1*/}
+          <span className='eoq absolute -top-1/2 bottom-0 left-0 right-0 z-[2] h-1/2 w-full bg-red  group-hover:top-0'></span>
+          {/* Filler 2 */}
+          <span className='eoq absolute bottom-0 left-0 right-0 top-full z-[2] h-1/2 w-full bg-red  group-hover:top-1/2'></span>
+        </Link>
       </div>
     </section>
   );
